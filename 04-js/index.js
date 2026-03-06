f41cb6415f362347b94e45c1941800ad6d1eb5bd
Puneeth78 <puneethv78@gmail.com>
2026-03-06 20:41:27 +0530

    mini project

---
+++ 04-js/index.js
@@ -0,0 +1,28 @@
+// variable scope =where a variable scope is recognisable and accessable (local and global)
+
+
+// local=any variable declared outside the function
+
+// function2();
+// function function1(){
+//     let x=1;
+//     console.log(x);
+// }
+// function1();
+ 
+// function function2(){
+//     let y=1;  //local variable
+//     console.log()
+    
+// }
+
+// // global variable=any variable decleare outside the function the function
+// let x1=1;  //global variable
+// function function12(){
+//     console.log(x1);
+// }
+// function12();
+
+
+
+

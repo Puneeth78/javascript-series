649bf10cd5fa638146cb6362c1a38c0ddaa8cb6c
Puneeth V <PuneethV78@gmail.com>
2026-03-06 20:43:19 +0530

    updated

--- 04-js/index.js
+++ 04-js/index.js
@@ -3,25 +3,25 @@
 
 // local=any variable declared outside the function
 
-// function2();
-// function function1(){
-//     let x=1;
-//     console.log(x);
-// }
-// function1();
+function2();
+function function1(){
+    let x=1;
+    console.log(x);
+}
+function1();
  
-// function function2(){
-//     let y=1;  //local variable
-//     console.log()
+function function2(){
+    let y=1;  //local variable
+    console.log()
     
-// }
+}
 
-// // global variable=any variable decleare outside the function the function
-// let x1=1;  //global variable
-// function function12(){
-//     console.log(x1);
-// }
-// function12();
+// global variable=any variable decleare outside the function the function
+let x1=1;  //global variable
+function function12(){
+    console.log(x1);
+}
+function12();
 
 
 
